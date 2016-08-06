ec196bfd84b4f4211120e3bd200ae9513160123a
johnsoncodehk <johnsoncodehk@gmail.com>
2016-08-06 18:28:50 +0800

    Johnson

--- backend/api.js
+++ backend/api.js
@@ -1,38 +1,73 @@
 var db = require('./db');
-
-module.exports.getPins = function (req, res, next) {
-    console.log('get pin list request');
-    console.log(req.query.lat);
-       console.log(req.query.lon);
-    res.status(200).send(req.params);
-    };
+var db2 = require('./db2'); // wrong
 
 module.exports.addPin = function (req, res, next) {
-    console.log(req.body);
-    res.status(200).send(req.body);
-}
-module.exports.getPinsByTags = function (req, res, next) {
     var params = rea.body;
-    var tags = params.tags;
+
+    var pin = params.pin;
+
+    db.addPin(pin);
+
+    sendResult(res);
+}
+module.exports.getPinsByLocation = function (req, res, next) {
+    var params = rea.body;
+
+    var location = params.location;
+    var radius = params.radius;
     var allPins = getAllPins();
 
     var resultPins = [];
     for (var i = 0; i < allPins.length; i++) {
         var pin = allPins[i];
-        for (var j = 0; j < tags.length; j++) {
-            var tag = tags[i];
-            if (pin.tags.indexOf(tag) > -1) {
-                resultTags.push(pin);
-            }
+        var d = db2.getd(pinLocation, location); // wrong
+        if (d <= radius) {
+            resultPins.push(pin);
+        }
+    }
+
+    sendResult(resultPins);
+}
+module.exports.getPinsByTag = function (req, res, next) {
+    var params = rea.body;
+
+    var tag = params.tag;
+    var allPins = getAllPins();
+
+    var resultPins = [];
+    for (var i = 0; i < allPins.length; i++) {
+        var pin = allPins[i];
+        if (pin.tags.indexOf(tag) > -1) {
+            resultPins.push(pin);
         }
     }
 
     sendResult(resultPins);
 };
+module.exports.likePin = function (req, res, next) {
+    var params = rea.body;
 
-function sendResult (res, result) {
+    var userId = params.userId;
+    var pinId = params.parId;
+
+    db.addPinLike(pinId); // wrong
+
+    sendResult(res);
+}
+module.exports.unlikePin = function (req, res, next) {
+    var params = rea.body;
+
+    var userId = params.userId;
+    var pinId = params.parId;
+
+    db.addPinUnlike(pinId); // wrong
+
+    sendResult(res);
+}
+
+function sendResult (res, result = {}) {
     res.status(200).send(result);
 }
-function sendError (res, message) {
+function sendError (res, message = "") {
     res.status(500).send(message);
 }
--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(router);
 
-router.get('/getPins', api.getPins);
 router.post('/addPin', api.addPin);
-router.post('/getPinsByTags', api.getPinsByTags);
+router.post('/getPinsByLocation', api.getPinsByLocation);
+router.post('/getPinsByTag', api.getPinsByTag);
+router.post('/likePin', api.likePin);
+router.post('/unlikePin', api.unlikePin);
 
 var server = require('http').createServer(app);
 server.listen(8888);
