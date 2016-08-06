73808312e2ac10e2500cf590ae4f994b5bde962e
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2016-08-06 21:08:35 +0800

    created database quey for addPin, getPins

--- backend/db.js
+++ backend/db.js
@@ -4,39 +4,82 @@ var Client = require('mariasql');
 module.exports.addPin = function (pin) {
 
     var deferred = Q.defer();
-
-    console.log('insert pin name:' + pin.name);
-
-
-    execute('INSERT INTO pin (name) VALUES (:name)', {name: pin.name},
+    
+    execute('INSERT INTO pin (title, name, userId, lat, lon) ' +
+        'VALUES (:title, :name, : userId, :lat, :lon)',
+        {name: pin.name},
         function (err, rows) {
-            if (err)
-                throw err;
-            deferred.resolve(rows);
+            if (err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(rows);
+            }
         });
-
 
     return deferred.promise;
 };
 
-module.exports.addPin1 = function (pin) {
+module.exports.getPins = function () {
 
     var deferred = Q.defer();
 
-
-    execute('SELECT * FROM pin WHERE id = :id', {id: 1},
-        function (err, rows) {
-            if (err)
-                throw err;
+    getAllPins(function (err, rows) {
+        if (err) {
+            deferred.reject(err);
+        } else {
             deferred.resolve(rows);
-        });
-
+        }
+    });
 
     return deferred.promise;
 };
 
-function execute(query, param, cb) {
 
+module.exports.getPinsByTag = function (tag) {
+
+    var deferred = Q.defer();
+
+    getAllPins(function (err, rows) {
+        if (err) {
+            deferred.reject(err);
+        } else {
+            deferred.resolve(rows);
+        }
+    });
+    
+    
+
+    return deferred.promise;
+};
+
+function getTags() {
+    
+}
+
+function getAllPins(cb) {
+    return execute('SELECT ' +
+        'pin.id, ' +
+        'pin.title, ' +
+        'pin.message, ' +
+        'pin.userId, ' +
+        'pin.lat, ' +
+        'pin.lon ' +
+        'FROM pin', cb);
+}
+
+function execute(query, param, cb) {
+    c = connect();
+    c.query(query, param, cb);
+    c.end();
+}
+
+function execute(query, cb) {
+    c = connect();
+    c.query(query, cb);
+    c.end();
+}
+
+function connect() {
     var c = new Client();
     c.connect({
         host: '127.0.0.1',
@@ -53,7 +96,5 @@ function execute(query, param, cb) {
         .on('close', function (hadError) {
             console.log('Client closed');
         });
-
-    c.query(query, param, cb);
-    c.end();
+    return c;
 }
