9ef33b42a4aea23abb54e1c3686759d152600398
dragonairex <dragonairex@gmail.com>
2016-08-07 07:56:33 +0800

    removed custom marker; still adding stuff

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -1,80 +1,5 @@
 var GlobalPinsJSON=[];
 
-//CUSTOM MARKER CODE (FROM GITHUB)
-function CustomMarker(latlng, map, args) {
-	this.latlng = latlng;
-	this.args = args;
-	this.setMap(map);
-}
-
-CustomMarker.prototype = new google.maps.OverlayView();
-
-CustomMarker.prototype.draw = function() {
-
-	var self = this;
-
-	var div = this.div;
-
-	if (!div) {
-
-		div = this.div = document.createElement('div');
-
-		div.className = 'pin tag1';
-		div.style.position = 'absolute';
-
-		if (typeof(self.args.user_id) !== 'undefined') {
-			div.dataset.user_id = self.args.user_id;
-		}
-		if (typeof(self.args.title) !== 'undefined') {
-			div.dataset.title = self.args.title;
-		}
-		if (typeof(self.args.message) !== 'undefined') {
-			div.dataset.message = self.args.message;
-		}
-		if (typeof(self.args.lat) !== 'undefined') {
-			div.dataset.lat = self.args.lat;
-		}
-		if (typeof(self.args.lon) !== 'undefined') {
-			div.dataset.lon = self.args.lon;
-		}
-		var userIdThing;
-		if (typeof(self.args.user_id) !== 'undefined') {
-			div.dataset.user_id = self.args.user_id;
-			userIdThing=self.args.user_id;
-		}
-
-		google.maps.event.addDomListener(div, "click", function(event) {
-			//google.maps.event.trigger(self, "click");
-			event.stopPropagation();
-			showPinMessage(this);
-		});
-
-		this.div.innerHTML="<div><img src='images/"+userIdThing+".jpg' alt='' onerror='this.src=\'images/undefined.jpg\''></img></div>";
-
-		var panes = this.getPanes();
-		panes.overlayImage.appendChild(div);
-	}
-
-	var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
-
-	if (point) {
-		div.style.left = point.x + 'px';
-		div.style.top = point.y + 'px';
-	}
-};
-
-CustomMarker.prototype.remove = function() {
-	if (this.div) {
-		this.div.parentNode.removeChild(this.div);
-		this.div = null;
-	}
-};
-
-CustomMarker.prototype.getPosition = function() {
-	return this.latlng;
-};
-/****/
-
 var map;
 /*var dummyPins={"pins":[
 		{"title":"title1", "message":"some text here.","location":{"lat":22.283636353214973,"lon":114.1349458694458},"tags":["A","B","C"],"user_id":"user1"},
@@ -125,12 +50,12 @@ function showPosition(position) {
 }
 
 //Add new pin
-function addPin(lat,lon) {
+/*function addPin(lat,lon) {
 		console.log("Add pin: Latitude: " + lat + " Longitude: " + lon);
 
 	var myLatlng=new google.maps.LatLng(lat,lon);
 
-	var overlay = new CustomMarker(
+	var marker = new google.maps.Marker(
 		myLatlng,
 		map,
 		{
@@ -141,26 +66,26 @@ function addPin(lat,lon) {
 			lon:lon
 		}
 	);
-}
+}*/
 
 function addPinFromJSON(singlePin) {
 	//console.log("Add pin: Latitude: " + lat + " Longitude: " + lon);
 	var myLatlng=new google.maps.LatLng(singlePin.location.lat,singlePin.location.lon);
 
 	//var myLatlng=new google.maps.LatLng(22.283636353214973,114.1349458694458);
-	var overlay = new CustomMarker(
-		myLatlng,
-		map,
-		{
-			user_id: ''+singlePin.user_id+'',
-			title: ''+singlePin.title+'',
-			message: ''+singlePin.message+'',
-			lat:singlePin.location.lat,
-			lon:singlePin.location.lon
-		}
-	);
-	overlay.addListener('click', function(e) {
-		markerClick(this.LatLng);
+	var marker = new google.maps.Marker({
+		position:osition,
+		map:map,
+		icon:'images/icon.png'
+	});
+	var j={"user_id":singlePin.user_id,
+			"title":singlePin.title,
+			"message":singlePin.message,
+			"lat":singlePin.location.lat,
+			"lon":singlePin.location.lon
+	};
+	marker.addListener('click', function(e) {
+		markerClick(j);
 	});
 }
 //SHOW PIN MESSAGE
@@ -222,8 +147,8 @@ function placeMarker(position) {
 }
 
 //
-function markerClick(latLng){
-	console.log("click");
+function markerClick(j){
+	console.log(j);
 }
 
 //READ JSON AND CREATE ALL PINS
