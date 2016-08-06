9225fb25fe78d3c40a522fef55169d4aea4a4567
johnsoncodehk <johnsoncodehk@gmail.com>
2016-08-06 16:34:25 +0800

    Johnson

--- backend/api.js
+++ backend/api.js
@@ -1,7 +1,15 @@
-module.exports.getPinList = function (req, res, next) {
-console.log('get pin list request');
+module.exports.getPins = function (req, res, next) {
+console.log('get pin list request' );
+var respo = {
+
+}
+res.status(200).send("hi");
 };
 
-module.exports.getPinListByLocation = function (req, res, next) {
+module.exports.getPinsByLocation = function (req, res, next) {
 
+};
+
+module.exports.newPin = function (req, res, next) {
+res.status(300);
 };
--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,9 @@ var api = require('./api');
 
 var app = express(),
     router 	= express.Router();
-router.get('/:getPinList', api.getPinList);
-router.get('/:getPinListByLocation', api.getPinListByLocation);
+router.get('/:getPins', api.getPinList);
+router.get('/:getPinsByLocation', api.getPinListByLocation);
+router.post('/newPin', api.newPin)
 app.use(router);
 var server = require('http').createServer(app);
 server.listen(8888);
