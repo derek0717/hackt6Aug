acb0904ef352cc45f37b9081fb552dcb53678fc3
johnsoncodehk <johnsoncodehk@gmail.com>
2016-08-06 22:10:16 +0800

    Johnson

--- backend/api.js
+++ backend/api.js
@@ -107,7 +107,7 @@ function getWorkingPins (pins) {
 	return workingPins;
 }
 function checkPin(pin) {
-	return pin.userId >= 0
+	return pin.userId
 		&& pin.message
 		&& pin.message
 		&& pin.location
