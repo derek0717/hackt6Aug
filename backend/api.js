2e26b8bac33ae61b77264ff4c85d58ac54248733
johnsoncodehk <johnsoncodehk@gmail.com>
2016-08-06 20:19:56 +0800

    Johnson

--- backend/api.js
+++ backend/api.js
@@ -7,11 +7,10 @@ module.exports.addPin = function (req, res, next) {
     var pin = params.pin;
 
     db.addPin(pin).then(function (row) {
-        console.log(row);
+        sendResult(resultPins);
     }).fail(function (err) {
-        console.log(err);
+        sendError(err);
     });
-
 };
 
 module.exports.getPinsByLocation = function (req, res, next) {
@@ -19,34 +18,39 @@ module.exports.getPinsByLocation = function (req, res, next) {
 
     var location = params.location;
     var radius = params.radius;
-    var allPins = getAllPins();
 
-    var resultPins = [];
-    for (var i = 0; i < allPins.length; i++) {
-        var pin = allPins[i];
-        var d = db2.getd(pinLocation, location); // wrong
-        if (d <= radius) {
-            resultPins.push(pin);
-        }
-    }
-
-    sendResult(resultPins);
+    db.getAllPins.then(function (allPins) {
+        // var resultPins = [];
+        // for (var i = 0; i < allPins.length; i++) {
+        //     var pin = allPins[i];
+        //     var d = db2.getd(pinLocation, location); // wrong
+        //     if (d <= radius) {
+        //         resultPins.push(pin);
+        //     }
+        // }
+        // sendResult(resultPins);
+        sendResult(allPins);
+    }).fail(function (err) {
+        sendError(err);
+    });
 }
 module.exports.getPinsByTag = function (req, res, next) {
     var params = rea.body;
 
     var tag = params.tag;
-    var allPins = getAllPins();
 
-    var resultPins = [];
-    for (var i = 0; i < allPins.length; i++) {
-        var pin = allPins[i];
-        if (pin.tags.indexOf(tag) > -1) {
-            resultPins.push(pin);
+    db.getAllPins.then(function (allPins) {
+        var resultPins = [];
+        for (var i = 0; i < allPins.length; i++) {
+            var pin = allPins[i];
+            if (pin.tags.indexOf(tag) > -1) {
+                resultPins.push(pin);
+            }
         }
-    }
-
-    sendResult(resultPins);
+        sendResult(resultPins);
+    }).fail(function (err) {
+        sendError(err);
+    });
 };
 module.exports.likePin = function (req, res, next) {
     var params = rea.body;
@@ -54,9 +58,11 @@ module.exports.likePin = function (req, res, next) {
     var userId = params.userId;
     var pinId = params.parId;
 
-    db.addPinLike(pinId); // wrong
-
-    sendResult(res);
+    db.addPinLike(pinId).then(function () {
+        sendResult({});
+    }).fail(function (err) {
+        sendError(err);
+    });
 }
 module.exports.unlikePin = function (req, res, next) {
     var params = rea.body;
@@ -64,15 +70,17 @@ module.exports.unlikePin = function (req, res, next) {
     var userId = params.userId;
     var pinId = params.parId;
 
-    db.addPinUnlike(pinId); // wrong
-
-    sendResult(res);
+    db.addPinUnlike(pinId).then(function () {
+        sendResult({});
+    }).fail(function (err) {
+        sendError(err);
+    });
 }
 
-function sendResult (res, result = {}) {
+function sendResult (res, result) {
     res.status(200).send(result);
 }
 
-function sendError (res, message = "") {
+function sendError (res, message) {
     res.status(500).send(message);
 }
