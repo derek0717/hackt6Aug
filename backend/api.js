616bed234682d6560409adb806321354f62901c6
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2016-08-06 15:57:38 +0800

    initial backend commit

---
+++ backend/api.js
@@ -0,0 +1,7 @@
+module.exports.getPinList = function (req, res, next) {
+
+};
+
+module.exports.getPinListByLocation = function (req, res, next) {
+
+};
---
+++ backend/app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var api = require('api');
+
+var app = express(),
+    router 	= express.Router();
+router.get('/:getPinList', api.getPinList);
+router.get('/:getPinListByLocation', api.getPinListByLocation);
+app.use(router);
+var server = require('http').createServer(app);
+server.listen(8888);
