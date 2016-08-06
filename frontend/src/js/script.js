aa0945469a95f7910c313898e2541aa68ee9460d
ecDusty <ecmgit@outlook.com>
2016-08-06 18:20:02 +0800

    Map added to JS file

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -6,6 +6,17 @@ function getLocation() {
     }
 }
 function showPosition(position) {
-    var x = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude; 
+    var x = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
     console.log(x);
 }
+
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(22.286918099999998,114.1494163),
+    zoom:15,
+    disableDefaultUI:true,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
