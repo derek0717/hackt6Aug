6b90f48ab293ab8cee3fbe7ee7eb573d43444948
ecDusty <ecmgit@outlook.com>
2016-08-06 22:54:47 +0800

    Added the ability to test pins

--- frontend/src/js/spromise.js
+++ frontend/src/js/spromise.js
@@ -1,14 +1,4 @@
-/*
-var testPins = [
-		{"title":"title1", "message":"some text here.","location":{"lat":22.283636353214973,"lon":114.1349458694458},"tags":["A","B","C"],"user_id":"user1"},
-		{"title":"this is a title", "message":"Doe","location":{"lat":22.280181510711184,"lon":114.15121078491211},"tags":["A","B","C"],"user_id":"user1"},
-		{"title":"John", "message":"Doe","location":{"lat":22.290545782110424,"lon":114.14709091186523},"tags":["A","B","C"],"user_id":"user2"},
-]
-
-var testString = JSON.stringify(testPins);
-*/
-
-var pinsObject;
+var testThePins
 
 var theMainURL = "http://192.168.11.179:8888";
 // -> /getPins returns an array of pins
@@ -52,9 +42,14 @@ function get(url) {
 
 get(theMainURL+'/getPins').then(JSON.parse).then(function (response) {
 	var localJSON = response;
+	testThePins = response; // for testing only
 //	console.log(localJSON);
 	readJsonAndCreateAllPins(response);
 });
+
+function addtoJSON () {
+
+};
 
 //get('database/testing.json').then(function(response) {
 //  console.log("Success!", response);
